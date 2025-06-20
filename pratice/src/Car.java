b2d39e1cfec5478b39f6ae928a3d2e6afb657a40
Himanshu Singh <himansh.singh293@gmail.com>
2025-06-20 08:47:41 +0530

    initilised the var with values

--- pratice/src/Car.java
+++ pratice/src/Car.java
@@ -1,10 +1,12 @@
 public class Car {
-    private String make ;
-    private  String model ;
-    private  String color ;
-    private  String doors ;
+    private String make = "BMW";
+    private  String model = "MW-R8";
+    private  String color = "Black" ;
+    private  String doors = "4";
+    private  boolean convertible = true ;
 
     public  void  describeCar (){
-        System.out.println(doors +"- Door" + color + " " + model + " " + make );
+        System.out.println(doors +"- Door " + color + " " + model + " " + make + " is it convertible "+
+                (convertible ? "Yes":"No"));
     }
 }

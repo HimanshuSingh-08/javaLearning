057bbd8efb3397d0a84f3f8b7ea4a8e7b1471e50
Himanshu Singh <himansh.singh293@gmail.com>
2025-06-20 08:39:45 +0530

    Car class with just declaration

---
+++ pratice/src/Car.java
@@ -0,0 +1,10 @@
+public class Car {
+    private String make ;
+    private  String model ;
+    private  String color ;
+    private  String doors ;
+
+    public  void  describeCar (){
+        System.out.println(doors +"- Door" + color + " " + model + " " + make );
+    }
+}

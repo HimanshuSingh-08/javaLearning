384131a5451817900775d0514e04641e8ec3bb37
Himanshu Singh <himansh.singh293@gmail.com>
2025-06-20 09:07:13 +0530

    implemented the getter functions for the Car class

--- pratice/src/Car.java
+++ pratice/src/Car.java
@@ -2,8 +2,30 @@ public class Car {
     private String make = "BMW";
     private  String model = "MW-R8";
     private  String color = "Black" ;
-    private  String doors = "4";
+    private  int doors = 4;
     private  boolean convertible = true ;
+
+    //Settings functions to get values
+
+    public String getMake(){
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
 
     public  void  describeCar (){
         System.out.println(doors +"- Door " + color + " " + model + " " + make + " is it convertible "+

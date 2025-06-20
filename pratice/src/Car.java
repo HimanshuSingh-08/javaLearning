951f1448b4f2a8ee1e9d191cb538e15568220df0
Himanshu Singh <himansh.singh293@gmail.com>
2025-06-20 20:25:48 +0530

    added the setter functions for the car class

--- pratice/src/Car.java
+++ pratice/src/Car.java
@@ -27,6 +27,28 @@ public class Car {
         return convertible;
     }
 
+
+    //now we have to set the setter function incase to set the values
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
+    }
+
+    public void setMake(String make){
+        this.make = make;
+    }
     public  void  describeCar (){
         System.out.println(doors +"- Door " + color + " " + model + " " + make + " is it convertible "+
                 (convertible ? "Yes":"No"));
--- pratice/src/Main.java
+++ pratice/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Car car = new Car();
+        car.setColor("red-black");
+        car.setMake("Tata");
+        car.setDoors(2);
         System.out.println(car.getMake());
         car.describeCar();
     }

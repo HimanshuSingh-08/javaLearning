5c1a31c67c055971893f3236fd669d2d83720fc3
Himanshu Singh <himansh.singh293@gmail.com>
2025-06-27 23:31:31 +0530

    Imlmented computer building classes. Explored about the concept of Composite classes and methods

--- BuildingComputer/src/Main.java
+++ BuildingComputer/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ComputerCase theCase = new ComputerCase("2208", "Hp Omen");
+        Monitor theMonitor = new Monitor("27inch the beast and curved", "Acer", 27 ,"2540x1440");
+        Motherboard theMotherboard = new Motherboard("MSI-23" , "msi-dragon",4,6,"v2.23");
+
+        PersonalComputer personalComputer = new PersonalComputer("2208", "HP Omen",theMonitor,theMotherboard,theCase);
+
+        personalComputer.getMonitor().drawPixelAt(10,10,"blue");
+        personalComputer.getMotherboard().loadProgram("Windows OS is booting Up");
+        personalComputer.getComputerCase().pressPowerButton();
     }
 }

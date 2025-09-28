70467c7c8fd07da86db1dd163b431f226f63c589
Himanshu Singh <himansh.singh293@gmail.com>
2025-09-28 18:02:04 +0530

    All singleton classes implementation

--- GenericClass/src/Main.java
+++ GenericClass/src/Main.java
@@ -25,5 +25,114 @@ public class Main {
         for (SampleEnums sample : SampleEnums.values()){
             System.out.println("Actual day of week:" + sample + " and its numeric value " + sample.ordinal());
         }
+
+        //Enum usage of ValueOf() and name()
+        SampleEnums temp = SampleEnums.valueOf("WEDNESDAY") ;
+        System.out.print(temp.name());
     }
 }
+
+
+// Eager Initialization
+ class DBConnection {
+    // object created as soon as class loads
+    private static DBConnection conObject = new DBConnection();
+
+    private DBConnection(){}
+
+    public static DBConnection getInstance(){
+        return conObject;
+    }
+
+    public void connect(){
+        System.out.println("Connected using Eager Initialization!");
+    }
+}
+
+// Lazy Initialization
+class DBConnection {
+    private static DBConnection conObject;
+
+    private DBConnection(){}
+
+    public static DBConnection getInstance(){
+        if(conObject == null){
+            conObject = new DBConnection();
+        }
+        return conObject;
+    }
+
+    public void connect(){
+        System.out.println("Connected using Lazy Initialization!");
+    }
+}
+
+// Synchronized Method
+public class DBConnection {
+    private static DBConnection conObject;
+
+    private DBConnection(){}
+
+    public static synchronized DBConnection getInstance(){
+        if(conObject == null){
+            conObject = new DBConnection();
+        }
+        return conObject;
+    }
+
+    public void connect(){
+        System.out.println("Connected using Synchronized Method!");
+    }
+}
+
+// Double Checked Locking
+ class DBConnection {
+    private static volatile DBConnection conObject;
+
+    private DBConnection(){}
+
+    public static DBConnection getInstance(){
+        if(conObject == null){
+            synchronized(DBConnection.class){
+                if(conObject == null){
+                    conObject = new DBConnection();
+                }
+            }
+        }
+        return conObject;
+    }
+
+    public void connect(){
+        System.out.println("Connected using Double Checked Locking!");
+    }
+}
+
+//Bill Pugh Singleton
+public class DBConnection {
+    private DBConnection(){}
+
+    // static inner helper class
+    private static class Helper {
+        private static final DBConnection INSTANCE = new DBConnection();
+    }
+
+    public static DBConnection getInstance(){
+        return Helper.INSTANCE;
+    }
+
+    public void connect(){
+        System.out.println("Connected using Bill Pugh Singleton!");
+    }
+}
+
+// Enum Singleton
+
+public enum DBConnection {
+    INSTANCE;
+
+    public void connect(){
+        System.out.println("Connected using Enum Singleton!");
+    }
+}
+
+

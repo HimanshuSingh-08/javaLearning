531820a3e8564d9a63c6ebaa9631eab46436da14
HimanshuSingh-08 <himansh.singh293@gmail.com>
2026-01-03 18:26:21 +0530

    implemented bubble sort algorithim

---
+++ DSA/main/src/Sorting/BubbleSort.java
@@ -0,0 +1,27 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        // These algorithm are for sorting an existing
+        int[] arr = {23,43,4,5,11,29,30};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    //This is bubble sort algorithm
+    public static void sort(int[] arr ){
+        int len = arr.length;
+        for(int i=0; i<len; i++){
+            for(int j=1; j<len; j++){
+                if(arr[j-1] > arr[j]){
+                    int temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+}

80a2cd3a7f43d79fed7dcc5a61c26a6a2db61204
HimanshuSingh-08 <himansh.singh293@gmail.com>
2026-01-03 16:40:29 +0530

    implemented binary search on 1D array

---
+++ DSA/main/src/Search/binarySearch.java
@@ -0,0 +1,31 @@
+package Search;
+
+
+public class binarySearch {
+    public static void main(String[] args) {
+        int [] arr = {0,3,5,7,8,9,15,17,19};
+
+        System.out.println(searchIndex(23,arr));
+    }
+
+    //helper function to get the particular index
+    //This algorithm is called as binarySearch : Time complexity :log(n)
+
+    public static int searchIndex(int target, int [] arr){
+        int s = 0, e = arr.length-1;
+        while(s <= e){
+            int mid = s + (e-s)/2;
+
+            // if we get element in the array.
+            if(arr[mid] == target){
+                return mid;
+            }else if (arr[mid] < target){
+                s = mid +1;
+            }else{
+                e = mid -1;
+            }
+        }
+        // index is not there then directly return -1;
+        return -1;
+    }
+}

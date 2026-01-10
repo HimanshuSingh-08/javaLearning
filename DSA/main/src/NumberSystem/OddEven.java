d746406970eb7f3becd9de02c5ab180ad8730cf4
HimanshuSingh-08 <himansh.singh293@gmail.com>
2026-01-10 13:31:56 +0530

    implmeneted even and odd number check function using bits

--- DSA/main/src/NumberSystem/OddEven.java
+++ DSA/main/src/NumberSystem/OddEven.java
@@ -1,4 +1,18 @@
 package NumberSystem;
 
+import java.util.Scanner;
+
 public class OddEven {
+    public static void main(String[] args) {
+        // check if  a no is odd using bits
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Enter the no t check : ");
+        int num = inp.nextInt();
+
+        System.out.println(checkNumber(num));
+    }
+
+    public static boolean checkNumber(int n){
+        return ((n & 1) == 1);
+    }
 }

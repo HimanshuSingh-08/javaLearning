f8dd2e19e62d3bbfd78ae8db88a61265b3bcb814
HimanshuSingh-08 <himansh.singh293@gmail.com>
2026-01-17 15:39:16 +0530

    impelemented floor of a number in a sorted array

--- DSA/main/src/Search/ceilingNumber.java
+++ DSA/main/src/Search/ceilingNumber.java
@@ -6,7 +6,9 @@ public class ceilingNumber {
         int [] arr = {2,3,5,7,8,9,12,14,17};
 
         //helper function to find ceil of a number.
-        System.out.println(getCeilNumber(2, arr));
+//        System.out.println(getCeilNumber(2, arr));
+
+        System.out.println(getFloorNumber(7,arr));
     }
 
     //this helper function will return the ceil
@@ -25,5 +27,22 @@ public class ceilingNumber {
         }
         return arr[s];
     }
-    //
+
+    //Helper function to get the floor of function.
+    public static int getFloorNumber(int target , int [] arr){
+        int s = 0, e = arr.length - 1;
+        while(s<=e){
+            int mid = s + (e-s)/2;
+            // element found then return the index
+            if(arr[mid] == target){
+                System.out.println("Equal element is found");
+                return mid; // yaha pe hi element  mil jara hai isliye wo 5 dera hai.
+            }else if(arr[mid] < target){
+                s = mid + 1;
+            }else{
+                e = mid -1;
+            }
+        }
+        return arr[e];
+    }
 }

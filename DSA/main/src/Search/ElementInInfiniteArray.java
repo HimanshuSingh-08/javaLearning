f7577fc73a51851cf1cdd026cc86e1c3b21fefec
HimanshuSingh-08 <himansh.singh293@gmail.com>
2026-01-18 01:29:41 +0530

    Implimented code for finding element in a sorted infinite array

---
+++ DSA/main/src/Search/ElementInInfiniteArray.java
@@ -0,0 +1,48 @@
+package search;
+
+public class ElementInInfiniteArray {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9,12,13,16,17,19,20,23,24,27,30};
+        int target = 23;
+
+        int index = searchInInfiniteArray(arr, target);
+        System.out.println(index);
+    }
+
+    static int searchInInfiniteArray(int[] arr, int target) {
+        int start = 0;
+        int end = 1;
+
+        // Expand the range safely
+        while (end < arr.length && target > arr[end]) {
+            int newStart = end + 1;
+            end = end + (end - start + 1) * 2;
+
+            //why we did this : Reason being what if the end size got greater then the file then ?
+            // it will give the error and hence we have to make it in lower bound.
+            if (end >= arr.length) {
+                end = arr.length - 1;
+            }
+
+            start = newStart;
+        }
+
+        return binarySearch(arr, target, start, end);
+    }
+
+    static int binarySearch(int[] arr, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}

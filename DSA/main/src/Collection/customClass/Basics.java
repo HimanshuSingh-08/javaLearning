ddba05dde1cda0d7fbb8fff693acab1ac8350a24
HimanshuSingh-08 <himansh.singh293@gmail.com>
2026-01-25 13:03:58 +0530

    Removed extra class for internalData

--- DSA/main/src/Collection/customClass/Basics.java
+++ DSA/main/src/Collection/customClass/Basics.java
@@ -3,11 +3,9 @@ package Collection.customClass;
 class Data {
     public Integer num;
     public String name;
-    private Integer holding;
     Data(Integer num, String name, Integer revenue){
         this.num = num;
         this.name = name;
-        this.holding = revenue;
     }
 
     //getter and setter function for the custom data.
@@ -20,14 +18,7 @@ class Data {
     }
 
     public  void printData(){
-        System.out.println("This is my name: " + num + " and this is my number: " + name + "And this is my revenue" + holding);
-    }
-}
-
-class InternalData {
-    public Integer revenue;
-    InternalData(Integer _revenue){
-        this.revenue = _revenue;
+        System.out.println("This is my name: " + num + " and this is my number: " + name + "And this is my revenue");
     }
 }
 

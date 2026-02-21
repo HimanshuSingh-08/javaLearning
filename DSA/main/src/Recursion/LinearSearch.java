3ec03f596337e252e03f27ffed3659a816f2b86a
HimanshuSingh-08 <himansh.singh293@gmail.com>
2026-02-21 16:15:15 +0530

    Imlmeneted concept of in between storage creation for rescursive function

--- DSA/main/src/Recursion/LinearSearch.java
+++ DSA/main/src/Recursion/LinearSearch.java
@@ -1,9 +1,11 @@
 package Recursion;
 
+import java.util.ArrayList;
+
 public class LinearSearch {
     public static void main(String[] args) {
-        int [] arr = {1,23,5,2,3};
-        System.out.println(getTarget(arr , 0 , 50));
+        int [] arr = {1,23,5,2,3,3,3,5,6,7};
+        System.out.println(getTargetList(arr , 3 , 0));
     }
     //helper function to find target
     public static int getTarget(int [] arr ,int i, int target){
@@ -17,5 +19,25 @@ public class LinearSearch {
             return i;
         }
         return getTarget(arr , i+1,target);
+    }
+
+    // lets consider if want to push multiple entries of targets in that case we need to
+    // assume we want to create the issue then we have
+    public static ArrayList<Integer> getTargetList(int [] arr, int target , int i){
+        // we need store our answers.
+        ArrayList<Integer> ans = new ArrayList<>();
+        // if we reached to the end then it means we already sorted out and added the numbers.
+        if(i == arr.length-1){
+            return ans;
+        }
+
+        //if we found target then we will add to the list
+        if(arr[i] == target){
+            ans.add(arr[i]);
+        }
+
+        ArrayList<Integer> ansFromPrevCall = getTargetList(arr , target, i+1);
+        ans.addAll(ansFromPrevCall);
+        return ans;
     }
 }

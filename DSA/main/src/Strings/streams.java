82b9e561284d193391adef69797a6284e855d59e
HimanshuSingh-08 <himansh.singh293@gmail.com>
2026-02-28 23:56:06 +0530

    implemented streams in one argument

--- DSA/main/src/Strings/streams.java
+++ DSA/main/src/Strings/streams.java
@@ -3,12 +3,12 @@ package Strings;
 public class streams {
     public static void main(String[] args) {
         String str = "Himanshu";
-        skipChar(str,"");
+//        skipChar(str,"");
+        System.out.println(skipCharOneArg(str));
     }
 
     //helper function to process the strings
     public static void skipChar(String str , String ans){
-        
 
         if(str.isEmpty()){
             System.out.println("This is my processed str : " + ans);
@@ -23,6 +23,23 @@ public class streams {
             skipChar(str.substring(1) , ans);
         }else{
             skipChar(str.substring(1), ans + ch);
+        }
+    }
+    // if we wanted to do it in one argument in that case we.
+
+    public static String skipCharOneArg(String str){
+
+        if(str.isEmpty()){
+            return " ";
+        }
+        // rest of the code block me ham isko process karenge
+        char ch = str.charAt(0);
+
+        // now we have to check if our current char should be included or removed.
+        if(ch == 's' || ch == 'u'){
+            return skipCharOneArg(str.substring(1));
+        }else{
+            return ch + skipCharOneArg(str.substring(1));
         }
     }
 }

200198713c9a48cda42eb06c9d6cbdefb7cc4186
HimanshuSingh-08 <himansh.singh293@gmail.com>
2026-03-01 16:14:03 +0530

    implemented subseqList with arrayList as a output

--- DSA/main/src/Strings/subsets.java
+++ DSA/main/src/Strings/subsets.java
@@ -1,9 +1,12 @@
 package Strings;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class subsets {
     public static void main(String[] args) {
         String str = "abc";
-        getSubsets(str, "");
+        System.out.println(getSubseqList(str, ""));
     }
 
     // we need a helpe function to print all the subsets
@@ -18,5 +21,23 @@ public class subsets {
         char ch = str.charAt(0);
         getSubseq(str.substring(1) , ans + ch);
         getSubseq(str.substring(1) , ans);
+    }
+
+    // what if we want to return it and store it so in that case we need to send it as arraylist
+
+    public static ArrayList<String> getSubseqList(String str, String ans){
+        // if our str is empty in that case we will return
+        if(str.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(ans);
+            return list;
+        }
+
+        // now we have to simply either consider a char or not
+        char ch = str.charAt(0);
+        ArrayList<String> leftPart =  getSubseqList(str.substring(1) , ans + ch);
+        ArrayList<String> rightPart =  getSubseqList(str.substring(1) , ans);
+        leftPart.addAll(rightPart);
+        return leftPart;
     }
 }

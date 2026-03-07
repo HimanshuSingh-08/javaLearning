2f0d6ac3b0d670b42146575148e5223aa74434c2
HimanshuSingh-08 <himansh.singh293@gmail.com>
2026-03-07 13:47:13 +0530

    implmented to getall the ans in a list fomat

--- DSA/main/src/Strings/permutations.java
+++ DSA/main/src/Strings/permutations.java
@@ -1,10 +1,14 @@
 package Strings;
 // in this will create all possible combination that are their for us.
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class permutations {
     public static void main(String[] args) {
         String str = "abc";
-        printPermutations("" , str);
+        ArrayList<String> ans = getPermutations("",str);
+        System.out.println(ans);
 
     }
 
@@ -29,5 +33,30 @@ public class permutations {
 
             printPermutations(first + ch + last, unp.substring(1));
         }
+    }
+
+    // now if i want to get this data into form of list as i want to return back it.
+    public static ArrayList<String> getPermutations(String p , String unp){
+        // when our p becomes empty then it means we have used all the characters.
+        if (unp.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        // now the difference in this pattern is we are not aware of exact number of calls that we will make
+        // as per every level we are making n+1 calls.
+        //why charAt(0) Because for every function call we are getting shorten string from which starting char is removed.
+
+        char ch = unp.charAt(0);
+        ArrayList<String> ans = new ArrayList<>();
+        for (int i = 0; i <= p.length(); i++) {
+
+            String first = p.substring(0, i);
+            String last = p.substring(i);
+
+            ans.addAll(getPermutations(first + ch + last, unp.substring(1)));
+        }
+        return ans;
     }
 }

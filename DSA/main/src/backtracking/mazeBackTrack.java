44dd61bb0db2e5c318ed1134f09455195b72a302
HimanshuSingh-08 <himansh.singh293@gmail.com>
2026-03-08 18:48:29 +0530

    Implemented maze with restrictions and print all possible paths in a array and a possible path for it

--- DSA/main/src/backtracking/mazeBackTrack.java
+++ DSA/main/src/backtracking/mazeBackTrack.java
@@ -1,9 +1,13 @@
 package backtracking;
 
+import java.util.Arrays;
+
 public class mazeBackTrack {
     public static void main(String[] args) {
         boolean [][] maze = {{true,true,true},{true,true,true},{true,true,true}};
-        printAvailablePathsAllDirection("",maze,0,0);
+//        printAvailablePathsAllDirection("",maze,0,0);
+        int [][] path = new int[3][3];
+        printAvailablePathsAllDirection("",maze,0,0,0,path);
     }
 
     // Now till now we are moving
@@ -44,5 +48,46 @@ public class mazeBackTrack {
     // but in that case there are high chances ki jab ham dusre path try kare honge us kaise me ho sakta hai.
     // ki wo hamne false mark kardiya hai and there fore last recursive call hote waqt hame un changes ko revert karna hoga
     // this is why it is called as backtrack remove those changes while coming back to original function calls made.
+
+
+    // now if we want to return the arrays to print the solution in that case
+
+    public static void printAvailablePathsAllDirection(String p , boolean [][] maze, int row , int col, int steps,int[][] path){
+        // when i have reached to a bottom in that case i have to print
+        if(row == maze.length-1 && col == maze[0].length-1){
+            //is case me we have to print as ham or age nahi ja sakte hai
+            path[row][col] = steps;
+            for(int [] arr : path){
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println(p);
+            System.out.println();
+            return ;
+        }
+        // now in case if we dont want to include specific cell in path then in that case we just have to
+        // stop the recursion
+
+        if(!maze[row][col]) return;
+
+        // now we have to explore all the direction.
+        maze[row][col] = false;
+        path[row][col] = steps;
+        //move down
+        if(row < maze.length -1) printAvailablePathsAllDirection(p + "D", maze ,row +1, col,steps +1, path);
+
+        //move right
+        if(col < maze[0].length -1) printAvailablePathsAllDirection(p + "R", maze ,row, col+1,steps +1, path);
+
+        //move up
+        if(row >0) printAvailablePathsAllDirection(p+ "U", maze,row-1, col,steps +1, path);
+
+        //move back
+
+        if(col > 0) printAvailablePathsAllDirection(p+"B",maze, row, col-1,steps +1, path);
+
+        // revert kar har us change ko jo tune kiya hai
+        maze[row][col] = true;
+        path[row][col] = 0;
+    }
 
 }

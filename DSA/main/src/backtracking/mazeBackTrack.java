dcff381588a25460fc4b0b457d95f3d513a741e4
HimanshuSingh-08 <himansh.singh293@gmail.com>
2026-03-08 18:14:26 +0530

    Implemented backtracking for maze problem and printed all possible unique paths. Only thing is ki isme longest paths bhi hai so we are as of now considering those

---
+++ DSA/main/src/backtracking/mazeBackTrack.java
@@ -0,0 +1,48 @@
+package backtracking;
+
+public class mazeBackTrack {
+    public static void main(String[] args) {
+        boolean [][] maze = {{true,true,true},{true,true,true},{true,true,true}};
+        printAvailablePathsAllDirection("",maze,0,0);
+    }
+
+    // Now till now we are moving
+    public static void printAvailablePathsAllDirection(String p , boolean [][] maze, int row , int col){
+        // when i have reached to a bottom in that case i have to print
+        if(row == maze.length-1 && col == maze[0].length-1){
+            //is case me we have to print as ham or age nahi ja sakte hai
+            System.out.println(p);
+            return ;
+        }
+        // now in case if we dont want to include specific cell in path then in that case we just have to
+        // stop the recursion
+
+        if(!maze[row][col]) return;
+
+        // now we have to explore all the direction.
+        maze[row][col] = false;
+
+        //move down
+        if(row < maze.length -1) printAvailablePathsAllDirection(p + "D", maze ,row +1, col);
+
+        //move right
+        if(col < maze[0].length -1) printAvailablePathsAllDirection(p + "R", maze ,row, col+1);
+
+        //move up
+        if(row >0) printAvailablePathsAllDirection(p+ "U", maze,row-1, col);
+
+        //move back
+
+        if(col > 0) printAvailablePathsAllDirection(p+"B",maze, row, col-1);
+
+        // revert kar har us change ko jo tune kiya hai
+        maze[row][col] = true;
+    }
+
+    // now as we are free to move in any direction we have to make it ensure ki we should not go
+    // those cells again where we already been there we have to avoid those.
+    // but in that case there are high chances ki jab ham dusre path try kare honge us kaise me ho sakta hai.
+    // ki wo hamne false mark kardiya hai and there fore last recursive call hote waqt hame un changes ko revert karna hoga
+    // this is why it is called as backtrack remove those changes while coming back to original function calls made.
+
+}

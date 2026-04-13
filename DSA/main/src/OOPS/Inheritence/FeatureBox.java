f254cac3f588746da19b035ff3e0bba2378ac368
HimanshuSingh-08 <himansh.singh293@gmail.com>
2026-04-13 23:19:00 +0530

    Implemented basic inheritence for Feature Box

--- DSA/main/src/OOPS/Inheritence/FeatureBox.java
+++ DSA/main/src/OOPS/Inheritence/FeatureBox.java
@@ -8,4 +8,12 @@ public class FeatureBox extends Box{
     public FeatureBox(){
         this.weight = -1;
     }
+
+    //what if we want to initilise all the parameters
+    public FeatureBox(double weight , double h , double l, double w){
+        // now we have access to the current element of weight but we need to initilise others too.
+        // for that we need to initilise it in the base class for that we first call its constructor
+        super(h,l,w);
+        this.weight = weight;
+    }
 }
--- DSA/main/src/OOPS/Main.java
+++ DSA/main/src/OOPS/Main.java
@@ -7,5 +7,8 @@ public class Main {
     public static void main(String[] args) {
         FeatureBox cube2 = new FeatureBox();
         System.out.println(cube2.weight + " " + cube2.length);
+
+        FeatureBox cuboid = new FeatureBox(23.4, 2,3,4);
+        System.out.println("The weight of the cuboid is "+ cuboid.weight + "And dimensions are " + cuboid.length +" "+ cuboid.width + " " + cuboid.height);
     }
 }

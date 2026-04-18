07395828bce5caf08f08e5e522d13f726527d6df
HimanshuSingh-08 <himansh.singh293@gmail.com>
2026-04-18 12:30:24 +0530

    Implemented Sugar class

--- DSA/main/src/OOPS/AbstractClass/Person.java
+++ DSA/main/src/OOPS/AbstractClass/Person.java
@@ -7,3 +7,7 @@ abstract public class Person {
     abstract String name ();
     abstract String gender ();
 }
+
+
+// Notes : Abstract class can be hybrid it may have some methods implmented and some not
+// But the object we are creating out of it are bounded to have the implmentation of those.Else it will break.

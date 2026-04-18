9a0d30ed043b3b35d1d7a3438acd7dc5f195a5e3
HimanshuSingh-08 <himansh.singh293@gmail.com>
2026-04-18 12:31:06 +0530

    Implemented Sugar class

--- DSA/main/src/OOPS/AbstractClass/Himanshu.java
+++ DSA/main/src/OOPS/AbstractClass/Himanshu.java
@@ -1,18 +1,19 @@
 package OOPS.AbstractClass;
 
 public class Himanshu extends Person{
+     @Override
      int getAge(){
          return 25;
      }
-
+    @Override
      String career(){
          return "Security Engineer";
      }
-
+    @Override
      String name(){
          return "Himanshu";
      }
-
+    @Override
      String gender(){
          return "Male";
      }
--- DSA/main/src/OOPS/AbstractClass/Sugar.java
+++ DSA/main/src/OOPS/AbstractClass/Sugar.java
@@ -1,4 +1,21 @@
 package OOPS.AbstractClass;
 
-public class Sugar {
+public class Sugar extends Person{
+    @Override
+    int getAge(){
+        return 23;
+    }
+
+    @Override
+    String career(){
+        return "Doctor";
+    }
+
+    String gender(){
+        return "Female";
+    }
+
+    String name(){
+        return "Pari";
+    }
 }

c7e2a49fff8c0d547e9c28e2f6829cfdc30b635d
HimanshuSingh-08 <himansh.singh293@gmail.com>
2026-04-19 19:34:00 +0530

    Implemented my own custom classes

--- DSA/main/src/OOPS/ExceptionHandiling/Main.java
+++ DSA/main/src/OOPS/ExceptionHandiling/Main.java
@@ -1,4 +1,21 @@
 package OOPS.ExceptionHandiling;
 
 public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 0;
+        // But we can catch the error
+        try{
+            divide(a,b);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    static public int divide (int a , int b) throws MyException{
+        if(b==0){
+            throw new MyException("This is my custom Exception. This operation is invalid as we cannot divide no by 0");
+        }
+        return a/b;
+    }
 }
--- DSA/main/src/OOPS/ExceptionHandiling/MyException.java
+++ DSA/main/src/OOPS/ExceptionHandiling/MyException.java
@@ -1,4 +1,8 @@
 package OOPS.ExceptionHandiling;
 
-public class MyException {
+public class MyException extends Exception{
+    // need a constructor for it
+    MyException(String message){
+        super(message);
+    }
 }

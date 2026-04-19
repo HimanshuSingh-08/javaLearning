ff0e27547e5c4d5995d33ca9de687e49b9dd39a8
HimanshuSingh-08 <himansh.singh293@gmail.com>
2026-04-19 14:47:03 +0530

    implemented custom array list with all type support

--- DSA/main/src/OOPS/Generics/GenericArrayList.java
+++ DSA/main/src/OOPS/Generics/GenericArrayList.java
@@ -2,17 +2,16 @@ package OOPS.Generics;
 
 import java.util.Arrays;
 
-public class CustomArrayList {
-    // We have to start with certain parameters
-    private int [] data;
+public class GenericArrayList<T> {
+    private Object[] data;
     static private int DEFAULT_SIZE = 10;
     private int size = 0;
 
-    public CustomArrayList(){
-        data = new int[DEFAULT_SIZE];
+    public GenericArrayList(){
+        data = new Object[DEFAULT_SIZE];
     }
     //method to add element
-    public void add(int num){
+    public void add(T num){
         //first we have to check if do we have space
         if(isFull()){
             // we have to resize array and then put element
@@ -23,7 +22,7 @@ public class CustomArrayList {
 
     // function resize will create new array
     private void resize(){
-        int[] temp = new int [data.length*2];
+        Object[] temp = new Object [data.length*2];
         for(int i=0; i < data.length; i++){
             temp[i] = data[i];
         }
@@ -36,8 +35,8 @@ public class CustomArrayList {
     }
 
     //we need function to return a value at an index
-    public int get(int index){
-        return this.data[index];
+    public T get(int index){
+        return (T)this.data[index];
     }
 
     //function to know actual size of array
@@ -46,33 +45,21 @@ public class CustomArrayList {
     }
 
     //function to set a value at particular index
-    public void set(int index , int value){
+    public void set(int index , T value){
         //first we need to check if index is feasible
         if(index <= size){
-            this.data[index] = value;
+            this.data[index] = value; // no need to type case we are adding to uper one in the object it will be handled by it self.
         }
     }
     // we need function to remove elements
-    public int removed(){
+    public Object removed(){
         return this.data[--size];
     }
 
-   // print element
+    // print element
     @Override
     public String toString(){
         return "CustomArray list : " + "data= " + Arrays.toString(data) + " size " +
                 size;
     }
 }
-
-
-// What are the methods we actually need in a custom array that are there in
-// actual array
-// -> Add element
-// -> Element at any index
-// -> Delete any element (Try to reduce the size when elements are not there more optimised)
-// -> Get element at any index which is present
-
-//*/Issue with current customArraylist is that we can only store the integer
-//        so we need a way to store all types as in actual arraylist we just specify the datatype and
-//we can store that type of in it. Thats why need generics through wih/*
--- DSA/main/src/OOPS/Generics/Main.java
+++ DSA/main/src/OOPS/Generics/Main.java
@@ -2,9 +2,17 @@ package OOPS.Generics;
 
 public class Main {
     public static void main(String[] args) {
-        CustomArrayList list = new CustomArrayList();
+        GenericArrayList<Integer> list = new GenericArrayList<>();
         list.add(2);
         list.add(34);
         System.out.println(list);
+        System.out.println(list.getSize());
+
+        // lets test this for string
+        GenericArrayList<String> list2 = new GenericArrayList<>();
+        list2.add("Himanshu");
+        list2.add("Singh");
+        System.out.println(list2);
+        System.out.println(list2.getSize());
     }
 }

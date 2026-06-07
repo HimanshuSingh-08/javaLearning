26d4f4e352db119d183ddd7da5e45b3500e14da2
HimanshuSingh-08 <himansh.singh293@gmail.com>
2026-06-07 14:21:52 +0530

    implmeneted thread methods run , start , sleep

--- Multithreading/src/ThreadMethods.java
+++ Multithreading/src/ThreadMethods.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+public class ThreadMethods extends Thread{
+    @Override
+    public void run() {
+        // part where we do execution
+        for(int i = 0; i<5; i++){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Thread is running");
+        }
+    }
 
-public class ThreadMethods {
+    public static void main(String[] args) {
+        // Create one thread
+        ThreadMethods t1 = new ThreadMethods();
+        t1.start();
+    }
 }
